5c849d8c822efc8c84c3057458164f434e66d592
razackoff <razakovdev@gmail.com>
2024-06-04 18:20:01 +0300

    Update base models.

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -5,8 +5,8 @@ public class Book
     public string Id { get; set; }
     public string Title { get; set; }
     public decimal Price { get; set; }
-    public string FileId { get; set; }
-    public string ImageId { get; set; }
+    public string FileName { get; set; }
+    public string ImageName { get; set; }
     public string CategoryId { get; set; }
     public List<string> Authors { get; set; }
 }

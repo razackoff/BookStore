611eec4fa8b379a078ab305ba372f8f7f2bc5b4a
razackoff <razakovdev@gmail.com>
2024-06-04 18:20:42 +0300

    Add function to upload files.

--- BookStore/Services/BookService.cs
+++ BookStore/Services/BookService.cs
@@ -1,10 +1,11 @@
+using BookStore.DTOs;
 using BookStore.Exceptions;
 using BookStore.Models;
 using BookStore.Repositories;
 
 namespace BookStore.Services;
 
-public class BookService(IBookRepository bookRepository) : IBookService
+public class BookService(IBookRepository bookRepository, IWebHostEnvironment webHostEnvironment) : IBookService
 {
     public async Task<List<Book>> GetAllBooksAsync()
     {
@@ -21,14 +22,69 @@ public class BookService(IBookRepository bookRepository) : IBookService
         return book;
     }
 
-    public async Task AddBookAsync(Book book)
+    public async Task<string> AddBookAsync(CreateBookDto book)
     {
-        await bookRepository.AddAsync(book);
+        var _book = new Book();
+        _book.Id = Guid.NewGuid().ToString();
+        _book.Title = book.Title;
+        _book.Price = book.Price;
+        _book.FileName = "unavailable";
+        _book.ImageName = "unavailable";
+        _book.CategoryId = book.CategoryId;
+        _book.Authors = book.Authors;
+        await bookRepository.AddAsync(_book);
+        return _book.Id;
     }
 
-    public async Task UpdateBookAsync(Book book)
+    public async Task UploadPdf(string id, IFormFile file)
     {
+        var book = await GetBookByIdAsync(id);
+
+        var uniqueFileName = $"{Guid.NewGuid()}_{file.FileName}";
+        var uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "files");
+
+        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        await using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        book.FileName = uniqueFileName;
+        
         await bookRepository.UpdateAsync(book);
+    }
+
+    public async Task UploadImage(string id, IFormFile file)
+    {
+        var book = await GetBookByIdAsync(id);
+        
+        if (file == null || file.Length == 0)
+            throw new ArgumentException("File is empty");
+
+        var uniqueFileName = $"{Guid.NewGuid()}_{file.FileName}";
+        var uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+
+        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        await using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        book.ImageName = uniqueFileName;
+        
+        await bookRepository.UpdateAsync(book);
+    }
+
+    public async Task UpdateBookAsync(string id, UpdateBookDto book)
+    {
+        var _book = await GetBookByIdAsync(id);
+        _book.Title = book.Title ?? _book.Title;
+        _book.Price = book.Price.HasValue ? book.Price.Value : _book.Price;
+        _book.CategoryId = book.CategoryId ?? _book.CategoryId;
+        _book.Authors = book.Authors ?? _book.Authors;
+        await bookRepository.UpdateAsync(_book);
     }
 
     public async Task DeleteBookAsync(string id)

65a0b26a3c4fef80a5cead30a37ef5f8526b4249
razackoff <razakovdev@gmail.com>
2024-06-04 14:03:09 +0300

    Add app db context.

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    
+}
